ed4fdb31df1039cd59dd1f675a4cee2801331391
Raul Martinez <rauldeoran17@gmail.com>
2023-06-20 18:42:13 -0300

    nuevas reglas del gitignore

--- Datos/Envios/175/ejemplo.py
+++
@@ -1,7 +0,0 @@
-n = int(input(""))
-c = 0
-for i in range(0,n,1):
-    x = int(input(""))
-    if (x % 2) == 0:
-        c+=1
-print(c)
--- Datos/Problemas/75/solucion/solucion.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    int a; 
-    scanf("%i",&a);
-    printf("Valor Ingresado: %i",a);
-    return 0;
-}
